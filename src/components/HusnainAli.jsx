75c653331edf2508c9bd3bc77c0305abfea876d2
ha2548493-glitch <ha2548493@gmail.com>
2026-02-24 10:49:43 +0500

    adding

--- src/components/HusnainAli.jsx
+++ src/components/HusnainAli.jsx
@@ -4,9 +4,9 @@ const HusnainAli = () => {
   return (
     <div>
 <h1>Husnain Ali</h1>
-        <h1>
+        <h1>Now i am working on git hub</h1>
           
-                <h1 className='h1tag'>Husnain Ali</h1>
+                <h1 className='h1tag' id='ok'>Husnain Ali</h1>
                 <input type='text' placeholder='write here'/>
                 <p className='ptag'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci illo hic necessitatibus praesentium assumenda quisquam maiores numquam cupiditate, explicabo cum rerum corrupti tempora accusantium accusamus velit molestias impedit eveniet ab?
@@ -16,7 +16,11 @@ const HusnainAli = () => {
                 <h2>
                     Thankyou sir for your appreciation
                 </h2>
-        </h1>
+
+                <button onClick={"ok"}>
+                    Click Here
+                </button>
+    
         <input type='text' placeholder='umari'></input>
     </div>
   )
