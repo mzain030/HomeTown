4a9f1e49185e9f6aaa387f1c388fd8f8dc593736
mzain030 <mzainnaseer786@gmail.com>
2026-02-24 10:51:31 +0500

    ok

--- src/components/Ghugh.jsx
+++ src/components/Ghugh.jsx
@@ -6,13 +6,15 @@ const Ghugh = () => {
       <h1>Adeel</h1>
         Muhammad Zain Naseer
 <h1>Now i done my github</h1>
+usman khan
 where area you linkind id 
 <h2>Welcome boys</h2>
 <button>GITHUB</button>
 <h1>Noe i set mt linkin id</h1>
 <h2>Welcome boys</h2>
 <h1>ghugh adeel</h1>
-<input type='text'></input>
+<input type='text' placeholder='okok'></input>
+<input type='text' placeholder='write here'></input>
     </div>
   )
 }
