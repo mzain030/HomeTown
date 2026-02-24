42e9c05f7ed9edb05593fee75ec00397a158eb66
adeel <adeelghugh340@gmail.com>
2026-02-24 09:36:02 +0500

    adding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Zain from './components/Zain';
+import Ghugh from './components/Ghugh';
 
 function App() {
   return (
     <div className="App">
      <Zain/>
+     <Ghugh/>
     </div>
   );
 }
---
+++ src/components/Ghugh.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ghugh = () => {
+  return (
+    <div>
+        Ghugh
+
+    </div>
+  )
+}
+
+export default Ghugh
