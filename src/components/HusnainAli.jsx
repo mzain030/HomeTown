fef4d0d7eab775fadb0e5fbf1f8a03aa4ad28134
ha2548493-glitch <ha2548493@gmail.com>
2026-02-24 10:36:56 +0500

    adding

---
+++ src/components/HusnainAli.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HusnainAli = () => {
+  return (
+    <div>
+
+        <h1>
+                <h1 className='h1tag'>Husnain Ali</h1>
+                <input type='text' placeholder='write here'/>
+                <p className='ptag'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci illo hic necessitatibus praesentium assumenda quisquam maiores numquam cupiditate, explicabo cum rerum corrupti tempora accusantium accusamus velit molestias impedit eveniet ab?
+                </p>
+        </h1>
+    </div>
+  )
+}
+
+export default HusnainAli;
