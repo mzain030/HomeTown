ed0f708c8ca8f57ae59ae7aa2142231743a78099
Hunain <hunainmazhariqbal@gmail.com>
2026-02-24 10:32:17 +0500

    Hunain  updated file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Zain from './components/Zain';
 import Ghugh from './components/Ghugh';
+import Hunain from './components/Hunain';
 
 function App() {
   return (
     <div className="App">
      <Zain/>
      <Ghugh/>
+     <Hunain/>
     </div>
   );
 }
---
+++ src/components/Hunain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hunain = () => {
+    return (
+        <div>
+            Hunain
+            <h1>this is how i done my Github Job</h1>
+        </div>
+    )
+}
+
+export default Hunain
