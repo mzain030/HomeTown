1243a15abe39a037c51766299d93254fce1156a0
mzain030 <mzainnaseer786@gmail.com>
2026-02-24 10:44:03 +0500

    adeel

--- src/components/Ghugh.jsx
+++ src/components/Ghugh.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const Ghugh = () => {
   return (
     <div>
+      <h1>Adeel</h1>
         Muhammad Zain Naseer
 <h1>Now i done my github</h1>
+where area you linkind id 
 <h2>Welcome boys</h2>
+<button>GITHUB</button>
     </div>
   )
 }
